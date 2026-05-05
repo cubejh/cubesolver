495cfa2ad999ed600ccbd34b13ab552b0d0b2e2b
unknown <cuberjhcubing@gmail.com>
2026-05-06 03:13:25 +0800

    make mainsolver

--- solver/mainsolver.py
+++ solver/mainsolver.py
@@ -7,92 +7,88 @@ from tables.p2_table import TableManagerP2
 from solver.phase1solver import Phase1Solver
 from solver.phase2solver import Phase2Solver
 
-def test_with_phase1solver(scramble):
-    start_time = time.time()
-    tm = TableManager(folder="cube_data")
-    solver = Phase1Solver(tm)
-    cube = Cube.newcube()
-    
-    for m in parse_moves(scramble):
-        m.apply(cube)
+class CubeSolver:
+    def __init__(self, data_folder="cube_data"):
+        print("--- Initializing Cube Solver (Loading Tables) ---")
+        start_time = time.time()
+        
+        self.tm1 = TableManager(folder=data_folder)
+        self.tm2 = TableManagerP2(folder=data_folder)
+        
+        self.solver1 = Phase1Solver(self.tm1)
+        self.solver2 = Phase2Solver(self.tm2)
+        
+        print(f"Solver ready. Tables loaded in {time.time() - start_time:.2f}s\n")
 
-    print(f"Scramble: {scramble}")
-    solution = solver.solve(cube)
-    solve_time = time.time() - start_time
-    print(f"Solve Time: {solve_time:.4f} seconds\n")
-
-    #edge_string = "OCXLWKEHNPAB"
-    #corner_string = "CLIXPRBT"
-    #print(f"edge_string: {edge_string}")
-    #print(f"corner_string: {corner_string}")
-    #cube2 = from_piece_def_init(edge_string, corner_string)
-    #solver.solve(cube2)
-
-def full_solver(scramble):
-    tm1 = TableManager(folder="cube_data")
-    solver1 = Phase1Solver(tm1)
-    
-    tm2 = TableManagerP2(folder="cube_data")
-    solver2 = Phase2Solver(tm2)
-
-    cube = Cube.newcube()
-    scramble_moves = parse_moves(scramble)
-    for m in scramble_moves:
-        m.apply(cube)
-
-    print(f"Scramble: {scramble}")
-
-    solution1 = solver1.solve(cube)
-    
-    if solution1:
+    def solve(self, cube):
+        
+        # Phase 1
+        solution1 = self.solver1.solve(cube)
+        if not solution1 and solution1 != "": 
+             print("Failed to find Phase 1 solution.")
+             return None
+        
         p1_moves = parse_moves(solution1)
         for m in p1_moves:
             m.apply(cube)
+            
+        # Phase 2
+        solution2 = self.solver2.solve(cube)
+        if solution2 is None:
+            print("Failed to find Phase 2 solution.")
+            return None
 
-        solution2 = solver2.solve(cube)
-    else:
-        solution2 = None
-
-    if solution1 and solution2:
         full_solution = f"{solution1} {solution2}".strip()
-        full_solution = simplify_moves(full_solution)
+        full_solution = self.simplify_moves(full_solution)
+        
+        
         print(f"Full Solution: {full_solution}")
         print(f"Steps: {len(full_solution.split())}")
-    else:
-        print("Failed to find a complete solution.")
-    return full_solution
-
-def simplify_moves(move_str):
-    if not move_str:
-        return ""
-    
-    moves = move_str.split()
-    simplified = []
-    
-    for move in moves:
-        face = move[0]
-        if len(move) == 1:
-            amount = 1
-        elif move[1] == '2':
-            amount = 2
-        elif move[1] == "'":
-            amount = 3
-        else:
-            amount = 1
-
-        if simplified and simplified[-1][0] == face:
-            prev_face, prev_amount = simplified.pop()
-            new_amount = (prev_amount + amount) % 4
-            
-            if new_amount != 0:
-                simplified.append((prev_face, new_amount))
-        else:
-            simplified.append((face, amount))
-    
-    res = []
-    for f, a in simplified:
-        if a == 1: res.append(f)
-        elif a == 2: res.append(f + "2")
-        elif a == 3: res.append(f + "'")
         
-    return " ".join(res)
+        return full_solution
+
+    def solve_scramble(self, scramble):
+        """"""
+        print(f"Scramble: {scramble}")
+        cube = Cube.newcube()
+        for m in parse_moves(scramble):
+            m.apply(cube)
+        return self.solve(cube)
+
+    def solve_piece_def(self, edge_string, corner_string):
+        """"""
+        cube = from_piece_def_init(edge_string, corner_string)
+        return self.solve(cube)
+    
+    def solve_piece_orientation_def(self, edge_string, corner_string):
+        """"""
+        cube = from_piece_orient_init(edge_string, corner_string)
+        return self.solve(cube)
+
+    @staticmethod
+    def simplify_moves(move_str):
+        """"""
+        if not move_str: return ""
+        moves = move_str.split()
+        simplified = []
+        for move in moves:
+            face = move[0]
+            amount = 1
+            if len(move) > 1:
+                if move[1] == '2': amount = 2
+                elif move[1] == "'": amount = 3
+            
+            if simplified and simplified[-1][0] == face:
+                prev_face, prev_amount = simplified.pop()
+                new_amount = (prev_amount + amount) % 4
+                if new_amount != 0:
+                    simplified.append((prev_face, new_amount))
+            else:
+                simplified.append((face, amount))
+        
+        res = []
+        for f, a in simplified:
+            if a == 1: res.append(f)
+            elif a == 2: res.append(f + "2")
+            elif a == 3: res.append(f + "'")
+        return " ".join(res)
--- test.py
+++ test.py
@@ -1,10 +1,23 @@
 import time
-from solver.mainsolver import full_solver
+from solver.mainsolver import CubeSolver
+"""
+def test_with_phase1solver(scramble):
+    start_time = time.time()
+    tm = TableManager(folder="cube_data")
+    solver = Phase1Solver(tm)
+    cube = Cube.newcube()
+    
+    for m in parse_moves(scramble):
+        m.apply(cube)
+
+    print(f"Scramble: {scramble}")
+    solution = solver.solve(cube)
+    solve_time = time.time() - start_time
+    print(f"Solve Time: {solve_time:.4f} seconds\n")
+"""
 
 start_time = time.time()
-solution = full_solver("R' U' F U2 F R2 B2 F2 D' R2 D' L2 D L2 R2 F2 B' R' U L2 D2 R U' B' R' U' F")
-solution = full_solver("R' U' F D2 U2 B2 R U2 F2 R D2 B2 L B' F D' B2 R D2 L' B D' B2 R' U' F")
-solution = full_solver("R' U' F U R' F2 U' L' D' B L D' U2 L2 B' R2 B D2 F2 L2 D2 L2 U2 B2 R' U' F")
-solution = full_solver("R' U' F D2 L B L2 B' F L2 R2 F' U2 L2 R2 U' R' U2 L2 D' R2 F' D2 R' U' F")
+cubesolver = CubeSolver()
+solution = cubesolver.solve_scramble("F L2 F R2 F2 U2 L2 F' L2 D2 L2 B2 L' D' B2 D' F' R2 D' B L")
 solve_time = time.time()-start_time
 print(f"==========\nSolve Time: {solve_time:.4f}\n==========")
